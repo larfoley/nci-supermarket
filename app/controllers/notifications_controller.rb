813ddee247764ce52a04a0c8d102199918ab163a
Laurence <larfoley@yahoo.com>
2020-04-02 20:26:30 +0100

    update devise styles

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,17 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = Notification.all
+    @notifications = []
+
+    # Filtering duplicate notifications due to bug with rails observers
+    # after_commit being called twice.
+    # More info here https://rails.lighthouseapp.com/projects/8994/tickets/764-observers-on-a-model-are-called-multiple-times
+    notifs = Notification.all
+
+    for n in notifs
+      if n[:id] % 2 == 0 then
+        @notifications.push(n)
+      end
+    end
+
   end
 end
