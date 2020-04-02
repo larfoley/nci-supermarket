5b9065389af7535a05207ada7fc0ae9d51d9edd3
Laurence <larfoley@yahoo.com>
2020-04-02 19:06:59 +0100

    update migrations

---
+++ db/migrate/20200317143358_create_products.rb
@@ -0,0 +1,12 @@
+class DropProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock
+
+      t.timestamps
+    end
+  end
+end
